5a843efaad58ebf5fe15b33d3ee175eb7d9cad13
Hyungjun <ryanromaris@naver.com>
2022-07-07 23:14:48 +0900

    ✨ feat(popularboard): 더미데이퍼 패칭, 인기 게시물 보드 완성

--- src/components/common-components/item/PopularBoard.tsx
+++ src/components/common-components/item/PopularBoard.tsx
@@ -1,87 +1,33 @@
 import { useEffect, useState } from 'react';
 
 const PopularBoard = () => {
-	const [aa, setaa] = useState([]);
+	const [popularArticle, setPopularArticle] = useState([]);
 	useEffect(() => {
 		fetch('https://jsonplaceholder.typicode.com/posts')
 			.then(response => response.json())
-			.then(json => setaa(json));
+			.then(json => {
+				//인기 게시물은 서버로부터 10개의 데이터만 받는다고 가정.
+				const popularArticleData = json.slice(0, 4);
+				setPopularArticle(popularArticleData);
+			});
 	}, []);
 
-	console.log(aa);
+	const truncateTitle = (title: string) => title.slice(0, 15) + '...';
 
 	return (
 		<>
-			{aa && (
+			{popularArticle && (
 				<>
 					<div className="popular_board_container">
 						<div className="popular_board_title">인기 게시물</div>
 						<div className="popular_board_content_box x mandatory-scroll-snapping">
-							{/* {console.log(typeof aa[0].title)} */}
-							{/* {aa.map(content => (
+							{popularArticle.map(content => (
 								<div className="popular_board_content">
-									<div className="popular_board_content_title">{content?.title}</div>
+									<div className="popular_board_content_title">{truncateTitle(content?.title)}</div>
+									<div className="popular_board_content_body">{truncateTitle(content?.body)}</div>
 								</div>
-							))} */}
-						</div>
-					</div>
-					<div className="popular_board_container">
-						<div className="popular_board_title">인기 게시물</div>
-						<div className="popular_board_content_box x mandatory-scroll-snapping">
-							<div className="popular_board_content">
-								<div className="popular_board_content_title">
-									제목입니다.
-									<br />
-									ㅁㅁㅁㅁㅁㅁㅁㅁㅁasdasdasdasdasㅁㅁㅁㅁ
-								</div>
-								<div className="popular_board_content_body">
-									내용입니다.ㅁㅁㅁㅁㅁㅁㅁasdasdasdasdsadasasddasㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ
-								</div>
-							</div>
-							<div className="popular_board_content">
-								<div className="popular_board_content_title">
-									제목입니다.ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ
-								</div>
-								<div className="popular_board_content_body">
-									내용입니다.ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ
-								</div>
-							</div>
-							<div className="popular_board_content">
-								<div className="popular_board_content_title">
-									제목입니다.ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ
-								</div>
-								<div className="popular_board_content_body">
-									내용입니다.ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ \aaa
-								</div>
-							</div>
-							<div className="popular_board_content">
-								<div className="popular_board_content_title">
-									제목입니다.ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ
-								</div>
-								<div className="popular_board_content_body">
-									내용입니다.ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ
-								</div>
-							</div>
-						</div>
-					</div>
-					<div className="popular_board_container">
-						<div className="popular_board_title">인기 게시물</div>
-						<div className="popular_board_content_box x mandatory-scroll-snapping">
-							<div className="popular_board_content"></div>
-							<div className="popular_board_content"></div>
-							<div className="popular_board_content"></div>
-							<div className="popular_board_content"></div>
-							<div className="popular_board_content"></div>
-						</div>
-					</div>
-					<div className="popular_board_container">
-						<div className="popular_board_title">인기 게시물</div>
-						<div className="popular_board_content_box x mandatory-scroll-snapping">
-							<div className="popular_board_content"></div>
-							<div className="popular_board_content"></div>
-							<div className="popular_board_content"></div>
-							<div className="popular_board_content"></div>
-							<div className="popular_board_content"></div>
+							))}
+							<div>더보기</div>
 						</div>
 					</div>
 				</>

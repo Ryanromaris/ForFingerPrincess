106aa3bdd8d09177c7e401f0d12bddfe9d49da20
Hyungjun <ryanromaris@naver.com>
2022-07-11 20:11:56 +0900

    ✨ feat(board): expand categories

---
+++ src/pages/board/index.tsx
@@ -0,0 +1,13 @@
+import Layout from '../../components/layout/Layout';
+
+const Board = ({ category = 'Hair' }) => {
+	return (
+		<>
+			<Layout>
+				<div>Hair Page</div>
+			</Layout>
+		</>
+	);
+};
+
+export default Board;

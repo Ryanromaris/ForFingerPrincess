3aadeffad725eb50286144a958969ea0210e4c6a
Hyungjun <ryanromaris@naver.com>
2022-11-12 14:55:56 +0900

    feat(search.tsx): study recoil selector

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,19 +1,29 @@
-import { atom, useRecoilState } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import Layout from 'src/components/layout/Layout';
-import { counting } from '../store';
+import { counting, selectArticle } from '../store';
 
 const Search = () => {
 	const [count, setCount] = useRecoilState(counting);
 
-	// console.log('recoild count :', count);
 	const handleIncrease = () => {
 		setCount(prev => prev + 1);
 	};
 
+	const selectArticles = useRecoilValue(selectArticle);
+
 	return (
 		<Layout>
-			<span>{count}</span>
-			<button onClick={handleIncrease}>increase</button>
+			<>
+				<span>{count}</span>
+				<button onClick={handleIncrease}>increase</button>
+
+				{selectArticles.map(article => (
+					<>
+						<div>id: {article.id}</div>
+						<div>content: {article.content}</div>
+					</>
+				))}
+			</>
 		</Layout>
 	);
 };
--- src/store.ts
+++ src/store.ts
@@ -1,4 +1,4 @@
-import { atom } from 'recoil';
+import { atom, selector } from 'recoil';
 
 export const user = atom({
 	key: 'user',
@@ -20,4 +20,18 @@ export const article = atom({
 		{ id: 2, content: 'test2', author: 'Jerry' },
 		{ id: 3, content: 'test3', author: 'Jerry' },
 	],
+});
+
+export const selectStatus = atom({
+	key: 'nowStatus',
+	default: 1,
+});
+
+export const selectArticle = selector({
+	key: 'selectArticle',
+	get: ({ get }) => {
+		const originalToDos = get(article);
+		const nowStatus = get(selectStatus);
+		return originalToDos.filter(article => article.id > nowStatus);
+	},
 });
